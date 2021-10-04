a2cf3b46f943f6820015dd2eeb489127047857f9
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-10-04 11:09:02 +0800

    Add tests for ModBookTime

---
+++ src/test/java/seedu/address/model/module/ModBookTimeTest.java
@@ -0,0 +1,44 @@
+package seedu.address.model.module;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ModBookTimeTest {
+
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> new ModBookTime(null));
+    }
+
+    @Test
+    public void constructor_invalidEmail_throwsIllegalArgumentException() {
+        String invalidTime = "";
+        assertThrows(IllegalArgumentException.class, () -> new ModBookTime(invalidTime));
+    }
+
+    @Test
+    void isValidTime() {
+        // null time
+        assertThrows(NullPointerException.class, () -> ModBookTime.isValidTime(null));
+
+        // blank time
+        assertFalse(ModBookTime.isValidTime(""));
+        assertFalse(ModBookTime.isValidTime(" "));
+
+        // invalid time formats
+        assertFalse(ModBookTime.isValidTime("4:00pm")); // use of AM/PM
+        assertFalse(ModBookTime.isValidTime("4pm")); // no minutes, use of AM/PM
+        assertFalse(ModBookTime.isValidTime("4:00")); // hours using 1 digit and not 2
+        assertFalse(ModBookTime.isValidTime("16")); // no minutes
+        assertFalse(ModBookTime.isValidTime("25:00")); // hours out of range
+        assertFalse(ModBookTime.isValidTime("14:98")); // minutes out of range
+
+        // valid times
+        assertTrue(ModBookTime.isValidTime("16:00"));
+        assertTrue(ModBookTime.isValidTime("08:00"));
+        assertTrue(ModBookTime.isValidTime("23:59"));
+    }
+}
