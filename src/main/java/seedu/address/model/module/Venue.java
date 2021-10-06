e0240e26b9a2d0f3a1b37c17b8f6aaf25fca8362
itzblim <>
2021-10-06 10:09:00 +0800

    Implement Venue class

--- src/main/java/seedu/address/model/module/Venue.java
+++ src/main/java/seedu/address/model/module/Venue.java
@@ -1,4 +1,55 @@
 package seedu.address.model.module;
 
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
 public class Venue {
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Venues should not be blank";
+
+    /*
+     * The first character of the venue must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "^\\S.*$";
+
+    public final String fullVenue;
+
+    /**
+     * Constructs a {@code Venue}.
+     *
+     * @param venue A valid venue.
+     */
+    public Venue(String venue) {
+        requireNonNull(venue);
+        checkArgument(isValidVenue(venue), MESSAGE_CONSTRAINTS);
+        fullVenue = venue;
+    }
+
+    /**
+     * Returns true if a given string is a valid venue.
+     */
+    public static boolean isValidVenue(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+
+    @Override
+    public String toString() {
+        return fullVenue;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Venue // instanceof handles nulls
+                && fullVenue.equals(((Venue) other).fullVenue)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return fullVenue.hashCode();
+    }
+
 }
