7578d1af07cfeeb82f83cb92d4c6eee2c48c0caf
itzblim <>
2021-10-06 09:41:45 +0800

    Implement Exam class

--- src/main/java/seedu/address/model/module/Exam.java
+++ src/main/java/seedu/address/model/module/Exam.java
@@ -1,4 +1,105 @@
 package seedu.address.model.module;
 
-public class Exam {
+import java.util.Optional;
+
+import seedu.address.model.person.Name;
+
+
+public class Exam implements Comparable<Exam>{
+
+    private final Name name;
+    private final ModBookDate date;
+    private final Timeslot timeslot;
+    private final Optional<Venue> venue;
+    private final Optional<Link> link;
+
+
+    public Exam(Name name, ModBookDate date, Timeslot timeslot, Optional<Venue> venue, Optional<Link> link) {
+        this.name = name;
+        this.date = date;
+        this.timeslot = timeslot;
+        this.venue = venue;
+        this.link = link;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public ModBookDate getDate() {
+        return date;
+    }
+
+    public Timeslot getTimeslot() {
+        return timeslot;
+    }
+
+    public Optional<Venue> getVenue() {
+        return venue;
+    }
+
+    public Optional<Link> getLink() {
+        return link;
+    }
+
+
+    /**
+     * Returns true if both exams have the same name.
+     * This defines a weaker notion of equality between two exams.
+     */
+    public boolean isSameExam(Exam otherExam) {
+        if (otherExam == this) {
+            return true;
+        }
+
+        return otherExam != null
+                && otherExam.getName().equals(getName());
+    }
+
+    /**
+     * Returns true if both exams have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Exam)) {
+            return false;
+        }
+
+        Exam otherPerson = (Exam) other;
+        return otherPerson.getName().equals(getName())
+                && otherPerson.getDate().equals(getDate())
+                && otherPerson.getTimeslot().equals(getTimeslot())
+                && otherPerson.getVenue().equals(getVenue())
+                && otherPerson.getLink().equals(getLink());
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Date: ")
+                .append(getDate())
+                .append("; Timeslot: ")
+                .append(getTimeslot())
+                .append("; Venue: ")
+                .append(getVenue())
+                .append("; Link: ")
+                .append(getLink());
+
+        return builder.toString();
+    }
+
+    @Override
+    public int compareTo(Exam otherExam) {
+        int dateOrder = date.compareTo(otherExam.date);
+        if (dateOrder == 0) {
+            return timeslot.compareTo(otherExam.timeslot);
+        }
+        return dateOrder;
+    }
 }
