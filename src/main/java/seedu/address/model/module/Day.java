10daeb9ef250e38e84b2112d96a98dcda750cf96
bentanjunrong <bentanjunrong@gmail.com>
2021-10-07 15:56:12 +0800

    Add validation checks for Day enum

--- src/main/java/seedu/address/model/module/Day.java
+++ src/main/java/seedu/address/model/module/Day.java
@@ -1,5 +1,17 @@
 package seedu.address.model.module;
 
 public enum Day {
-    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+
+    public static final String MESSAGE_CONSTRAINTS =
+            "Input for Day is not formatted correctly.";
+
+    public static boolean isValidDay(String dayString) {
+        try {
+            Day.valueOf(dayString);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
 }
