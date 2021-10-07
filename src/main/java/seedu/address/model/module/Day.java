138efa06e903bc754757755bcc6ecc88b4877465
bentanjunrong <bentanjunrong@gmail.com>
2021-10-07 18:53:32 +0800

    Change Day validity checking to be more flexible to case-sensitivity

--- src/main/java/seedu/address/model/module/Day.java
+++ src/main/java/seedu/address/model/module/Day.java
@@ -1,7 +1,7 @@
 package seedu.address.model.module;
 
 public enum Day {
-    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday;
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
 
     public static final String MESSAGE_CONSTRAINTS =
             "Input for Day is not formatted correctly.";
@@ -11,7 +11,7 @@ public enum Day {
      */
     public static boolean isValidDay(String dayString) {
         try {
-            Day.valueOf(dayString);
+            Day.valueOf(dayString.toUpperCase());
             return true;
         } catch (IllegalArgumentException e) {
             return false;
