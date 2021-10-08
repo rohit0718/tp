57e49d63b1aeea52979c4a276fe991ec523d640e
Rohit <rajeshbhatrohit@gmail.com>
2021-10-08 16:10:31 +0800

    add AppUtil tests for areListsEqual method

--- src/test/java/seedu/address/commons/util/AppUtilTest.java
+++ src/test/java/seedu/address/commons/util/AppUtilTest.java
@@ -1,11 +1,18 @@
 package seedu.address.commons.util;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class AppUtilTest {
+    private static final List<Integer> LIST_ONE = List.of(1,2);
+    private static final List<Integer> LIST_TWO = List.of(1,2,3);
+    private static final List<Integer> LIST_THREE = List.of(2,3,4);
 
     @Test
     public void getImage_exitingImage() {
@@ -32,5 +39,20 @@ public class AppUtilTest {
     public void checkArgument_falseWithErrorMessage_throwsIllegalArgumentException() {
         String errorMessage = "error message";
         assertThrows(IllegalArgumentException.class, errorMessage, () -> AppUtil.checkArgument(false, errorMessage));
+    }
+
+    @Test
+    public void checkAreListsEqual_diffSizeLists_returnsFalse() {
+        assertFalse(() -> AppUtil.areListsEqual(LIST_ONE, LIST_TWO));
+    }
+
+    @Test
+    public void checkAreListsEqual_sameSizeDiffElementsLists_returnsFalse() {
+        assertFalse(() -> AppUtil.areListsEqual(LIST_TWO, LIST_THREE));
+    }
+
+    @Test
+    public void checkAreListsEqual_sameSizeSameElementsLists_returnsTrue() {
+        assertTrue(() -> AppUtil.areListsEqual(LIST_TWO, LIST_TWO));
     }
 }
