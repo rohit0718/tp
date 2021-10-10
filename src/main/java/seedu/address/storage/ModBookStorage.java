655185f46e7705c1ec5567ccbd8e07962291723c
itzblim <>
2021-10-11 00:02:58 +0800

    Add methods to ModBookStorage interface

--- src/main/java/seedu/address/storage/ModBookStorage.java
+++ src/main/java/seedu/address/storage/ModBookStorage.java
@@ -1,4 +1,45 @@
 package seedu.address.storage;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.ReadOnlyModBook;
+
+/**
+ * Represents a storage for {@link seedu.address.model.ModBook}.
+ */
 public interface ModBookStorage {
+
+    /**
+     * Returns the file path of the data file.
+     */
+    Path getModBookFilePath();
+
+    /**
+     * Returns ModBook data as a {@link ReadOnlyModBook}.
+     *   Returns {@code Optional.empty()} if storage file is not found.
+     * @throws DataConversionException if the data in storage is not in the expected format.
+     * @throws IOException if there was any problem when reading from the storage.
+     */
+    Optional<ReadOnlyModBook> readModBook() throws DataConversionException, IOException;
+
+    /**
+     * @see #getModBookFilePath()
+     */
+    Optional<ReadOnlyModBook> readModBook(Path filePath) throws DataConversionException, IOException;
+
+    /**
+     * Saves the given {@link ReadOnlyModBook} to the storage.
+     * @param modBook cannot be null.
+     * @throws IOException if there was any problem writing to the file.
+     */
+    void saveModBook(ReadOnlyModBook modBook) throws IOException;
+
+    /**
+     * @see #saveModBook(ReadOnlyModBook)
+     */
+    void saveModBook(ReadOnlyModBook modBook, Path filePath) throws IOException;
+
 }
