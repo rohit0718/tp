9f3fdf8bda53821dd6b1aefdabb5f07b4b72c3f1
itzblim <>
2021-10-11 12:32:15 +0800

    Add tests for JsonModBookStorage

---
+++ src/test/java/seedu/address/storage/JsonModBookStorageTest.java
@@ -0,0 +1,110 @@
+package seedu.address.storage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalModules.CS2030S;
+import static seedu.address.testutil.TypicalModules.CS2103T;
+import static seedu.address.testutil.TypicalModules.MA1521;
+import static seedu.address.testutil.TypicalModules.getTypicalModBook;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.model.ModBook;
+import seedu.address.model.ReadOnlyModBook;
+
+public class JsonModBookStorageTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonModBookStorageTest");
+
+    @TempDir
+    public Path testFolder;
+
+    @Test
+    public void readModBook_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> readModBook(null));
+    }
+
+    private java.util.Optional<ReadOnlyModBook> readModBook(String filePath) throws Exception {
+        return new JsonModBookStorage(Paths.get(filePath)).readModBook(addToTestDataPathIfNotNull(filePath));
+    }
+
+    private Path addToTestDataPathIfNotNull(String prefsFileInTestDataFolder) {
+        return prefsFileInTestDataFolder != null
+                ? TEST_DATA_FOLDER.resolve(prefsFileInTestDataFolder)
+                : null;
+    }
+
+    @Test
+    public void read_missingFile_emptyResult() throws Exception {
+        assertFalse(readModBook("NonExistentFile.json").isPresent());
+    }
+
+    @Test
+    public void read_notJsonFormat_exceptionThrown() {
+        assertThrows(DataConversionException.class, () -> readModBook("notJsonFormatModBook.json"));
+    }
+
+    @Test
+    public void readModBook_invalidModuleModBook_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readModBook("invalidModuleModBook.json"));
+    }
+
+    @Test
+    public void readModBook_invalidAndValidModuleModBook_throwDataConversionException() {
+        assertThrows(DataConversionException.class, () -> readModBook("invalidAndValidModuleModBook.json"));
+    }
+
+    @Test
+    public void readAndSaveModBook_allInOrder_success() throws Exception {
+        Path filePath = testFolder.resolve("TempModBook.json");
+        ModBook original = getTypicalModBook();
+        JsonModBookStorage jsonModBookStorage = new JsonModBookStorage(filePath);
+
+        // Save in new file and read back
+        jsonModBookStorage.saveModBook(original, filePath);
+        ReadOnlyModBook readBack = jsonModBookStorage.readModBook(filePath).get();
+        assertEquals(original, new ModBook(readBack));
+
+        // Modify data, overwrite exiting file, and read back
+        original.addModule(CS2103T);
+        original.removeModule(CS2030S);
+        jsonModBookStorage.saveModBook(original, filePath);
+        readBack = jsonModBookStorage.readModBook(filePath).get();
+        assertEquals(original, new ModBook(readBack));
+
+        // Save and read without specifying file path
+        original.addModule(MA1521);
+        jsonModBookStorage.saveModBook(original); // file path not specified
+        readBack = jsonModBookStorage.readModBook().get(); // file path not specified
+        assertEquals(original, new ModBook(readBack));
+
+    }
+
+    @Test
+    public void saveModBook_nullModBook_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveModBook(null, "SomeFile.json"));
+    }
+
+    /**
+     * Saves {@code modBook} at the specified {@code filePath}.
+     */
+    private void saveModBook(ReadOnlyModBook modBook, String filePath) {
+        try {
+            new JsonModBookStorage(Paths.get(filePath))
+                    .saveModBook(modBook, addToTestDataPathIfNotNull(filePath));
+        } catch (IOException ioe) {
+            throw new AssertionError("There should not be an error writing to the file.", ioe);
+        }
+    }
+
+    @Test
+    public void saveModBook_nullFilePath_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> saveModBook(new ModBook(), null));
+    }
+}
