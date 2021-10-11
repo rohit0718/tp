3a86d5c30c5f6377386199b9d904e7cd2da45a15
Rohit <rajeshbhatrohit@gmail.com>
2021-10-11 17:42:13 +0800

    update input filepath name

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -70,6 +70,11 @@ public class LogicManager implements Logic {
     }
 
     @Override
+    public Path getModBookFilePath() {
+        return model.getModBookFilePath();
+    }
+
+    @Override
     public GuiSettings getGuiSettings() {
         return model.getGuiSettings();
     }
