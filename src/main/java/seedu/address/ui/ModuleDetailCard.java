aabcff37de28ed113ecdc7da148142f542a9b99f
Rohit <rajeshbhatrohit@gmail.com>
2021-10-11 21:06:47 +0800

    add initial ModuleDetailCard

---
+++ src/main/java/seedu/address/ui/ModuleDetailCard.java
@@ -0,0 +1,75 @@
+package seedu.address.ui;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import seedu.address.model.module.Module;
+
+/**
+ * An UI component that displays information of a {@code Module}'s details.
+ */
+public class ModuleDetailCard extends UiPart<Region> {
+
+    private static final String FXML = "ModuleSummaryCard.fxml";
+
+    /**
+     * Note: Certain keywords such as "location" and "resources" are reserved keywords in JavaFX.
+     * As a consequence, UI elements' variable names cannot be set to such keywords
+     * or an exception will be thrown by JavaFX during runtime.
+     *
+     * @see <a href="https://github.com/se-edu/addressbook-level4/issues/336">The issue on AddressBook level 4</a>
+     */
+
+    public final Module module;
+
+    @FXML
+    private HBox cardPane;
+    @FXML
+    private Label name;
+    @FXML
+    private Label id;
+    @FXML
+    private Label nextLesson;
+    @FXML
+    private Label nextExam;
+    @FXML
+    private FlowPane tags;
+
+    /**
+     * Creates a {@code ModuleCode} with the given {@code Module} and index to display.
+     */
+    public ModuleDetailCard(Module module) {
+        super(FXML);
+        this.module = module;
+        String moduleHeader = module.getCode().toString();
+
+        if (module.getName().isPresent()) {
+            moduleHeader += String.format(" %s", module.getName().get());
+        }
+
+        name.setText(moduleHeader);
+        nextLesson.setText(String.format("Next Lesson: %s", module.getNextLesson()));
+        nextExam.setText(String.format("Next Exam: %s", module.getNextExam()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        // short circuit if same object
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ModuleDetailCard)) {
+            return false;
+        }
+
+        // state check
+        ModuleDetailCard card = (ModuleDetailCard) other;
+        return id.getText().equals(card.id.getText())
+                && module.equals(card.module);
+    }
+}
