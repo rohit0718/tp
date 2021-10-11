dac55f2ffd1d3f96905fd4a4f6d950e2b2d4caa4
itzblim <>
2021-10-11 10:25:51 +0800

    Implement tests for JsonAdaptedModule

--- src/test/java/seedu/address/storage/JsonAdaptedModuleTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedModuleTest.java
@@ -1,4 +1,136 @@
 package seedu.address.storage;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.storage.JsonAdaptedLesson.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.Assert.assertThrows;
+import static seedu.address.testutil.TypicalModules.CS2103T;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.module.Day;
+import seedu.address.model.module.Link;
+import seedu.address.model.module.Module;
+import seedu.address.model.module.ModuleCode;
+import seedu.address.model.module.ModuleName;
+import seedu.address.model.module.Timeslot;
+import seedu.address.model.module.Venue;
+import seedu.address.model.module.exam.Exam;
+import seedu.address.model.module.lesson.Lesson;
+import seedu.address.model.module.lesson.LessonName;
+
 public class JsonAdaptedModuleTest {
+    private static final String INVALID_CODE = "CS421S";
+    private static final Optional<String> INVALID_NAME = Optional.of(" ");
+
+    private static final String VALID_CODE = CS2103T.getCode().toString();
+    private static final Optional<String> VALID_NAME = CS2103T.getName().map(ModuleName::toString);
+    private static final List<JsonAdaptedLesson> VALID_LESSONS = CS2103T.getLessons().stream()
+            .map(JsonAdaptedLesson::new).collect(Collectors.toList());
+    private static final List<JsonAdaptedExam> VALID_EXAMS = CS2103T.getExams().stream()
+            .map(JsonAdaptedExam::new).collect(Collectors.toList());
+    private static final Optional<String> EMPTY_NAME = Optional.empty();
+    private static final List<JsonAdaptedLesson> EMPTY_LESSONS = new ArrayList<>();
+    private static final List<JsonAdaptedExam> EMPTY_EXAMS = new ArrayList<>();
+
+    @Test
+    public void toModelType_validModuleDetails_returnsModule() throws Exception {
+        JsonAdaptedModule module = new JsonAdaptedModule(CS2103T);
+        assertEquals(CS2103T, module.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidCode_throwsIllegalValueException() {
+        JsonAdaptedModule module =
+                new JsonAdaptedModule(INVALID_CODE, VALID_NAME, VALID_LESSONS, VALID_EXAMS);
+        String expectedMessage = ModuleCode.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullCode_throwsIllegalValueException() {
+        JsonAdaptedModule module =
+                new JsonAdaptedModule(null, VALID_NAME, VALID_LESSONS, VALID_EXAMS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                ModuleCode.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        JsonAdaptedModule module =
+                new JsonAdaptedModule(VALID_CODE, INVALID_NAME, VALID_LESSONS, VALID_EXAMS);
+        String expectedMessage = ModuleName.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_emptyName_success() {
+        try {
+            JsonAdaptedModule module =
+                    new JsonAdaptedModule(VALID_CODE, EMPTY_NAME, VALID_LESSONS, VALID_EXAMS);
+            Module readModule = module.toModelType();
+            assertEquals(Optional.empty(), readModule.getName());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        JsonAdaptedModule module =
+                new JsonAdaptedModule(VALID_CODE, null, VALID_LESSONS, VALID_EXAMS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                ModuleName.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullLessons_throwsIllegalValueException() {
+            JsonAdaptedModule module =
+                    new JsonAdaptedModule(VALID_CODE, VALID_NAME, null, VALID_EXAMS);
+            String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                    List.class.getSimpleName());
+            assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_emptyLessons_success() {
+        try {
+            JsonAdaptedModule module =
+                    new JsonAdaptedModule(VALID_CODE, VALID_NAME, EMPTY_LESSONS, VALID_EXAMS);
+            Module readModule = module.toModelType();
+            assertEquals(new ArrayList<Lesson>(), readModule.getLessons());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
+    @Test
+    public void toModelType_nullExams_throwsIllegalValueException() {
+        JsonAdaptedModule module =
+                new JsonAdaptedModule(VALID_CODE, VALID_NAME, VALID_LESSONS, null);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT,
+                List.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, module::toModelType);
+    }
+
+    @Test
+    public void toModelType_emptyExams_success() {
+        try {
+            JsonAdaptedModule module =
+                    new JsonAdaptedModule(VALID_CODE, VALID_NAME, VALID_LESSONS, EMPTY_EXAMS);
+            Module readModule = module.toModelType();
+            assertEquals(new ArrayList<Exam>(), readModule.getExams());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
 }
