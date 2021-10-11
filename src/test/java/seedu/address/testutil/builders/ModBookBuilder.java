fb07bd128dc7142f79e17f659582cb5eb2487f58
itzblim <>
2021-10-11 08:50:17 +0800

    Implement methods for ModBookBuilder

--- src/test/java/seedu/address/testutil/builders/ModBookBuilder.java
+++ src/test/java/seedu/address/testutil/builders/ModBookBuilder.java
@@ -1,4 +1,34 @@
 package seedu.address.testutil.builders;
 
+import seedu.address.model.ModBook;
+import seedu.address.model.module.Module;
+
+/**
+ * A utility class to help with building ModBook objects.
+ * Example usage: <br>
+ *     {@code ModBook mb = new ModBookBuilder().withModule("CS2103T", "CS2040S").build();}
+ */
 public class ModBookBuilder {
+
+    private ModBook modBook;
+
+    public ModBookBuilder() {
+        modBook = new ModBook();
+    }
+
+    public ModBookBuilder(ModBook modBook) {
+        this.modBook = modBook;
+    }
+
+    /**
+     * Adds a new {@code Module} to the {@code ModBook} that we are building.
+     */
+    public ModBookBuilder withModule(Module module) {
+        modBook.addModule(module);
+        return this;
+    }
+
+    public ModBook build() {
+        return modBook;
+    }
 }
