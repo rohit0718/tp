61be33444f18c9b8d6b0f382431f7a5a8fa4efc1
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-10-12 14:38:39 +0800

    Add tests for DetailCommandParser

---
+++ src/test/java/seedu/address/logic/parser/DetailCommandParserTest.java
@@ -0,0 +1,47 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.CommandTestUtil.INVALID_MODULE_CODE_DESC;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_CODE;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_MODULE_CODE_DESC;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.DetailCommand;
+import seedu.address.model.module.ModuleCode;
+import seedu.address.model.module.ModuleHasModuleCodePredicate;
+
+public class DetailCommandParserTest {
+
+    private DetailCommandParser parser = new DetailCommandParser();
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "    ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DetailCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_missingKeyword_throwsParseException() {
+        assertParseFailure(parser, "cs", String.format(MESSAGE_INVALID_COMMAND_FORMAT, DetailCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidModuleCode_throwsParseException() {
+        assertParseFailure(parser, INVALID_MODULE_CODE_DESC, ModuleCode.MESSAGE_CONSTRAINTS);
+    }
+
+    @Test
+    public void parse_validArgs_returnsDetailCommand() {
+        // no leading and trailing whitespaces
+        DetailCommand expectedDetailCommand =
+                new DetailCommand(new ModuleHasModuleCodePredicate(new ModuleCode(VALID_MODULE_CODE)));
+        assertParseSuccess(parser, VALID_MODULE_CODE_DESC, expectedDetailCommand);
+
+        // leading and trailing whitespaces
+        assertParseSuccess(parser, " " + PREFIX_CODE + " \n \t " + VALID_MODULE_CODE
+                + " \n \t", expectedDetailCommand);
+    }
+}
