5f2c406620312c6d3af40f84acaea5b7bc8fb956
Rohit <rajeshbhatrohit@gmail.com>
2021-10-13 21:06:31 +0800

    add areprefixespresent method in parserutil

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
@@ -35,7 +36,7 @@ import seedu.address.model.tag.Tag;
 public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
-    public static final String MESSAGE_NO_INDEXES_FOUND = "No indexes found in command.";
+    public static final String MESSAGE_NO_INDEXES_FOUND = "Index not found in command.";
 
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
@@ -377,5 +378,13 @@ public class ParserUtil {
         default:
             throw new ParseException(errorMessage);
         }
+    }
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    public static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
     }
 }
