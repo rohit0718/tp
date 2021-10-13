d61b8ae7ec7ef428fa312830c7f1e983a448b698
Shezad <shezadhassan22@gmail.com>
2021-10-13 17:53:45 +0800

    remove unneccessary abstract method

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -1,22 +1,13 @@
 package seedu.address.logic.commands;
 
-import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.Model;
-
 /**
  * Adds a person to the address book.
  */
 public abstract class AddCommand extends Command {
-
     public static final String COMMAND_WORD = "add";
-
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Adds a mod/lesson/exam to the mod book. "
             + "Please specify what you would like to add\n"
             + "Example:\n" + COMMAND_WORD + " mod <parameters>\n"
             + COMMAND_WORD + " lesson <parameters>\n"
             + COMMAND_WORD + " exam <parameters>\n";
-
-    @Override
-    public abstract CommandResult execute(Model model) throws CommandException;
-
 }
