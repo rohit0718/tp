a3eef726f4439a1a89a79ad05aad808aabebb78a
bentanjunrong <bentanjunrong@gmail.com>
2021-10-19 00:37:20 +0800

    Add util methods for getting EditDescriptorDetails for the 3 Command classes

--- src/test/java/seedu/address/testutil/PersonUtil.java
+++ src/test/java/seedu/address/testutil/PersonUtil.java
@@ -2,15 +2,23 @@ package seedu.address.testutil;
 
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DAY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_LINK;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_VENUE;
 
 import java.util.Set;
 
 import seedu.address.logic.commands.EditCommand.EditPersonDescriptor;
 import seedu.address.logic.commands.add.AddCommand;
+import seedu.address.logic.commands.edit.EditExamCommand;
+import seedu.address.logic.commands.edit.EditLessonCommand;
+import seedu.address.logic.commands.edit.EditModCommand;
 import seedu.address.model.module.Module;
 import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
@@ -44,7 +52,7 @@ public class PersonUtil {
         sb.append(PREFIX_EMAIL + person.getEmail().value + " ");
         sb.append(PREFIX_ADDRESS + person.getAddress().value + " ");
         person.getTags().stream().forEach(
-            s -> sb.append(PREFIX_TAG + s.tagName + " ")
+                s -> sb.append(PREFIX_TAG + s.tagName + " ")
         );
         return sb.toString();
     }
@@ -78,6 +86,44 @@ public class PersonUtil {
                 tags.forEach(s -> sb.append(PREFIX_TAG).append(s.tagName).append(" "));
             }
         }
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code EditModDescriptor}'s details.
+     */
+    public static String getEditModDescriptorDetails(EditModCommand.EditModDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getModuleName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.moduleName).append(" "));
+        descriptor.getModuleCode().ifPresent(code -> sb.append(PREFIX_CODE).append(code.moduleCode).append(" "));
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code EditLessonDescriptor}'s details.
+     */
+    public static String getEditLessonDescriptorDetails(EditLessonCommand.EditLessonDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullLessonName).append(" "));
+        descriptor.getDay().ifPresent(day -> sb.append(PREFIX_DAY).append(day).append(" "));
+        descriptor.getTimeslot().ifPresent(timeslot -> sb.append(PREFIX_START).append(timeslot.startTime.toString())
+                .append(" ").append(PREFIX_END).append(timeslot.endTime.toString()).append(" "));
+        descriptor.getLink().ifPresent(link -> sb.append(PREFIX_LINK).append(link).append(" "));
+        descriptor.getVenue().ifPresent(venue -> sb.append(PREFIX_VENUE).append(venue).append(" "));
+        return sb.toString();
+    }
+
+    /**
+     * Returns the part of command string for the given {@code EditExamDescriptor}'s details.
+     */
+    public static String getEditExamDescriptorDetails(EditExamCommand.EditExamDescriptor descriptor) {
+        StringBuilder sb = new StringBuilder();
+        descriptor.getName().ifPresent(name -> sb.append(PREFIX_NAME).append(name.fullExamName).append(" "));
+        descriptor.getDate().ifPresent(day -> sb.append(PREFIX_DAY).append(day).append(" "));
+        descriptor.getTimeslot().ifPresent(timeslot -> sb.append(PREFIX_START).append(timeslot.startTime.toString())
+                .append(" ").append(PREFIX_END).append(timeslot.endTime.toString()).append(" "));
+        descriptor.getLink().ifPresent(link -> sb.append(PREFIX_LINK).append(link).append(" "));
+        descriptor.getVenue().ifPresent(venue -> sb.append(PREFIX_VENUE).append(venue).append(" "));
         return sb.toString();
     }
 }
