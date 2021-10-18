a8373426ed1facb28289e241f52b0f260d054069
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-10-18 17:03:26 +0800

    Add and update tests for ModBookTime

--- src/test/java/seedu/address/model/module/ModBookTimeTest.java
+++ src/test/java/seedu/address/model/module/ModBookTimeTest.java
@@ -1,9 +1,12 @@
 package seedu.address.model.module;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalTime;
 import org.junit.jupiter.api.Test;
 
 class ModBookTimeTest {
@@ -29,10 +32,9 @@ class ModBookTimeTest {
         assertFalse(ModBookTime.isValidTime(" "));
 
         // invalid time formats
-        assertFalse(ModBookTime.isValidTime("4:00pm")); // use of AM/PM
-        assertFalse(ModBookTime.isValidTime("4pm")); // no minutes, use of AM/PM
-        assertFalse(ModBookTime.isValidTime("4:00")); // hours using 1 digit and not 2
+        assertFalse(ModBookTime.isValidTime("abcd")); // not a time
         assertFalse(ModBookTime.isValidTime("16")); // no minutes
+        assertFalse(ModBookTime.isValidTime("5:00")); // only one digit in hours
         assertFalse(ModBookTime.isValidTime("25:00")); // hours out of range
         assertFalse(ModBookTime.isValidTime("14:98")); // minutes out of range
         assertFalse(ModBookTime.isValidTime("16:00:01")); // including seconds value
@@ -42,5 +44,54 @@ class ModBookTimeTest {
         assertTrue(ModBookTime.isValidTime("16:00"));
         assertTrue(ModBookTime.isValidTime("08:00"));
         assertTrue(ModBookTime.isValidTime("23:59"));
+
+        // valid time formats
+        assertTrue(ModBookTime.isValidTime("23:59"));
+        assertTrue(ModBookTime.isValidTime("23.59"));
+        assertTrue(ModBookTime.isValidTime("2359"));
+        assertTrue(ModBookTime.isValidTime("1PM"));
+        assertTrue(ModBookTime.isValidTime("11PM"));
+        assertTrue(ModBookTime.isValidTime("1:59PM"));
+        assertTrue(ModBookTime.isValidTime("11:59PM"));
+        assertTrue(ModBookTime.isValidTime("1.59PM"));
+        assertTrue(ModBookTime.isValidTime("11.59PM"));
+
+    }
+
+    @Test
+    void tryParse() {
+        LocalTime expectedFourPm = LocalTime.parse("16:00");
+        LocalTime expectedElevenThirtyAm = LocalTime.parse("11:30");
+        // null time
+        assertThrows(NullPointerException.class, () -> ModBookTime.tryParse(null));
+
+        // blank time
+        assertNull(ModBookTime.tryParse(""));
+        assertNull(ModBookTime.tryParse(" "));
+
+        // invalid time formats
+        assertNull(ModBookTime.tryParse("14")); // no minutes
+        assertNull(ModBookTime.tryParse("25:00")); // hours out of range
+        assertNull(ModBookTime.tryParse("14:98")); // minutes out of range
+        assertNull(ModBookTime.tryParse("16:00:01")); // including seconds value
+        assertNull(ModBookTime.tryParse("16:00:01.1234")); // including seconds and decimal value
+        assertNull(ModBookTime.tryParse("abcd")); // not a time
+
+        // valid time formats
+        assertEquals(ModBookTime.tryParse("16:00"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("16.00"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("1600"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("4PM"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("04PM"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("4:00PM"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("04:00PM"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("4.00PM"), expectedFourPm);
+        assertEquals(ModBookTime.tryParse("04.00PM"), expectedFourPm);
+
+        assertEquals(ModBookTime.tryParse("11:30"), expectedElevenThirtyAm);
+        assertEquals(ModBookTime.tryParse("11.30"), expectedElevenThirtyAm);
+        assertEquals(ModBookTime.tryParse("1130"), expectedElevenThirtyAm);
+        assertEquals(ModBookTime.tryParse("11:30AM"), expectedElevenThirtyAm);
+        assertEquals(ModBookTime.tryParse("11.30AM"), expectedElevenThirtyAm);
     }
 }
