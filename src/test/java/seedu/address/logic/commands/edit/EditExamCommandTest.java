1c3a1ebb792b0cf040dc133e2a77e359dfb781c1
bentanjunrong <bentanjunrong@gmail.com>
2021-10-19 15:24:38 +0800

    Implement massive tests for EditExamCommand

--- src/test/java/seedu/address/logic/commands/edit/EditExamCommandTest.java
+++ src/test/java/seedu/address/logic/commands/edit/EditExamCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands.edit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showModuleAtIndex;
@@ -21,6 +20,8 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.edit.EditExamCommand.EditExamDescriptor;
+import seedu.address.model.AddressBook;
+import seedu.address.model.ModBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
@@ -35,7 +36,7 @@ import seedu.address.testutil.builders.ModuleBuilder;
  * {@code EditExamCommand}.
  */
 public class EditExamCommandTest {
-    private Model model = new ModelManager(getTypicalAddressBook(), getTypicalModBook(), new UserPrefs());
+    private final Model model = new ModelManager(getTypicalAddressBook(), getTypicalModBook(), new UserPrefs());
 
     @Test
     public void constructor_nullExam_throwsNullPointerException() {
@@ -57,15 +58,16 @@ public class EditExamCommandTest {
         if (editedExam.getVenue().isPresent()) {
             editExamDescriptor.setVenue(editedExam.getVenue().get());
         }
+
         EditCommand editCommand = new EditExamCommand(
                 INDEX_FIRST_EXAM, targetModule.getCode(), editExamDescriptor);
 
         String expectedMessage = String.format(EditExamCommand.MESSAGE_EDIT_EXAM_SUCCESS, editedExam.getName(),
                 targetModule.getCode());
 
-        ModelManager expectedModel = new ModelManager(
-                model.getAddressBook(), model.getModBook(), new UserPrefs());
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), model.getModBook(), new UserPrefs());
         expectedModel.setExam(targetModule, targetModule.getExams().get(0), editedExam);
+
         assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
@@ -73,18 +75,50 @@ public class EditExamCommandTest {
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
         Module targetModule = model.getFilteredModuleList().get(INDEX_FIRST_MODULE.getZeroBased());
         Index outOfBoundIndex = Index.fromZeroBased(targetModule.getExams().size() + 1);
+
         EditCommand editExamCommand = new EditExamCommand(outOfBoundIndex, targetModule.getCode(),
                 new EditExamDescriptor());
+
         assertCommandFailure(editExamCommand, model, Messages.MESSAGE_INVALID_EXAM_DISPLAYED_INDEX);
     }
 
     @Test
     public void execute_someFieldsSpecifiedUnfilteredList_success() {
+        Module targetModule = new ModuleBuilder(CS2103T).withExams(CS2103T_PRACTICAL).build();
+        Exam editedExam = new ExamBuilder(CS2103T_PRACTICAL).build();
+
+        // Removed Timeslot and Link Fields
+        EditExamDescriptor editExamDescriptor = new EditExamDescriptor();
+        editExamDescriptor.setName(editedExam.getName());
+        editExamDescriptor.setDate(editedExam.getDate());
+        if (editedExam.getVenue().isPresent()) {
+            editExamDescriptor.setVenue(editedExam.getVenue().get());
+        }
+
+        EditCommand editCommand = new EditExamCommand(
+                INDEX_FIRST_EXAM, targetModule.getCode(), editExamDescriptor);
+
+        String expectedMessage = String.format(EditExamCommand.MESSAGE_EDIT_EXAM_SUCCESS, editedExam.getName(),
+                targetModule.getCode());
+        ModelManager expectedModel = new ModelManager(model.getAddressBook(), model.getModBook(), new UserPrefs());
+        expectedModel.setExam(targetModule, targetModule.getExams().get(0), editedExam);
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_noFieldSpecifiedUnfilteredList_success() {
+        Module editedModule = model.getFilteredModuleList().get(INDEX_FIRST_MODULE.getZeroBased());
+        Exam editedExam = new ExamBuilder(CS2103T_PRACTICAL).build();
+        EditCommand editCommand = new EditExamCommand(INDEX_FIRST_EXAM, editedModule.getCode(),
+                new EditExamDescriptor());
 
+        String expectedMessage = String.format(EditExamCommand.MESSAGE_EDIT_EXAM_SUCCESS, editedExam.getName(),
+                editedModule.getCode());
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
+                new ModBook(model.getModBook()), new UserPrefs());
+
+        assertCommandSuccess(editCommand, model, expectedMessage, expectedModel);
     }
 
 
@@ -93,8 +127,8 @@ public class EditExamCommandTest {
         showModuleAtIndex(model, INDEX_FIRST_MODULE);
         Module targetModule = CS2103T.deepCopy();
         Exam examToEdit = targetModule.getExams().get(INDEX_FIRST_EXAM.getZeroBased());
-
         Exam editedExam = new ExamBuilder(CS2103T_PRACTICAL).build();
+
         EditExamDescriptor editExamDescriptor = new EditExamDescriptor();
         editExamDescriptor.setName(editedExam.getName());
         editExamDescriptor.setDate(editedExam.getDate());
@@ -112,28 +146,63 @@ public class EditExamCommandTest {
                 targetModule.getCode());
         Model expectedModel = new ModelManager(model.getAddressBook(), model.getModBook(), new UserPrefs());
         expectedModel.setExam(targetModule, examToEdit, editedExam);
-        showNoModule(expectedModel);
+
         assertCommandSuccess(editExamCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
-    public void execute_duplicatePersonUnfilteredList_failure() {
+    public void execute_duplicateExamUnfilteredList_throwsCommandException() {
+        Module firstModule = model.getFilteredModuleList().get(INDEX_FIRST_MODULE.getZeroBased());
+        Exam firstExam = firstModule.getExams().get(INDEX_FIRST_EXAM.getZeroBased());
 
+        EditExamDescriptor editExamDescriptor = new EditExamDescriptor();
+        editExamDescriptor.setName(firstExam.getName());
+        editExamDescriptor.setDate(firstExam.getDate());
+        editExamDescriptor.setTimeslot(firstExam.getTimeslot());
+        if (firstExam.getLink().isPresent()) {
+            editExamDescriptor.setLink(firstExam.getLink().get());
+        }
+        if (firstExam.getVenue().isPresent()) {
+            editExamDescriptor.setVenue(firstExam.getVenue().get());
+        }
+
+        EditCommand editCommand = new EditExamCommand(INDEX_SECOND_EXAM, firstModule.getCode(), editExamDescriptor);
+
+        assertCommandFailure(editCommand, model, EditExamCommand.MESSAGE_DUPLICATE_EXAM);
     }
 
     @Test
-    public void execute_duplicatePersonFilteredList_failure() {
+    public void execute_duplicateExamFilteredList_throwsCommandException() {
+        Module firstModule = model.getFilteredModuleList().get(INDEX_FIRST_MODULE.getZeroBased());
+        Exam firstExam = firstModule.getExams().get(INDEX_FIRST_EXAM.getZeroBased());
 
+        EditExamDescriptor editExamDescriptor = new EditExamDescriptor();
+        editExamDescriptor.setName(firstExam.getName());
+        editExamDescriptor.setDate(firstExam.getDate());
+        editExamDescriptor.setTimeslot(firstExam.getTimeslot());
+        if (firstExam.getLink().isPresent()) {
+            editExamDescriptor.setLink(firstExam.getLink().get());
+        }
+        if (firstExam.getVenue().isPresent()) {
+            editExamDescriptor.setVenue(firstExam.getVenue().get());
+        }
+
+        EditCommand editCommand = new EditExamCommand(INDEX_SECOND_EXAM, firstModule.getCode(), editExamDescriptor);
+
+        assertCommandFailure(editCommand, model, EditExamCommand.MESSAGE_DUPLICATE_EXAM);
     }
 
     @Test
-    public void execute_invalidPersonIndexUnfilteredList_failure() {
+    public void execute_invalidExamIndexUnfilteredList_throwsCommandException() {
+        Module firstModule = model.getFilteredModuleList().get(INDEX_FIRST_MODULE.getZeroBased());
+        Index outOfBoundIndex = Index.fromOneBased(firstModule.getExams().size() + 1);
+        Exam editedExam = new ExamBuilder(CS2103T_PRACTICAL).build();
+        EditExamDescriptor editExamDescriptor = new EditExamDescriptor();
+        editExamDescriptor.setName(editedExam.getName());
 
-    }
+        EditCommand editCommand = new EditExamCommand(outOfBoundIndex, firstModule.getCode(), editExamDescriptor);
 
-    @Test
-    public void execute_invalidIndexFilteredList_throwsCommandException() {
-
+        assertCommandFailure(editCommand, model, Messages.MESSAGE_INVALID_EXAM_DISPLAYED_INDEX);
     }
 
     @Test
@@ -175,16 +244,8 @@ public class EditExamCommandTest {
 
         // different descriptor -> returns false
         EditExamDescriptor editExamDescriptorTwo = new EditExamDescriptor(editExamDescriptor);
-        editExamDescriptorTwo.setName(new ExamName("random"));
+        editExamDescriptorTwo.setName(new ExamName("CHAMPBENROCKZ"));
         assertNotEquals(editExamFirstCommand, new EditExamCommand(INDEX_FIRST_EXAM, CS2103T.getCode(),
                 editExamDescriptorTwo));
-    }
-
-    /**
-     * Updates {@code model}'s filtered list to show no one.
-     */
-    private void showNoModule(Model model) {
-        model.updateFilteredModuleList(m -> false);
-        assertTrue(model.getFilteredModuleList().isEmpty());
     }
 }
