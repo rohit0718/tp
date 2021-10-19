b3bc9ae428b9da9e50f531d6baae03e2b7368bf7
Rohit <rajeshbhatrohit@gmail.com>
2021-10-19 21:45:04 +0800

    remove unused imports

--- src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteCommandParser.java
@@ -1,8 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_CODE;
-import static seedu.address.logic.parser.ParserUtil.arePrefixesPresent;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.GuiState;
@@ -12,7 +10,6 @@ import seedu.address.logic.commands.delete.DeleteLessonCommand;
 import seedu.address.logic.commands.delete.DeleteModCommand;
 import seedu.address.logic.parser.exceptions.GuiStateException;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.module.ModuleCode;
 
 /**
  * Parses input arguments and creates a new DeleteCommand object
