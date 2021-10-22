aadda15a1c4b228ab624fb66ee809751d88448d0
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-10-22 21:45:18 +0800

    Update tests

--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -3,7 +3,10 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.DetailCommand;
 import seedu.address.logic.commands.GuiState;
+import seedu.address.logic.commands.list.ListCommand;
+import seedu.address.logic.parser.exceptions.GuiStateException;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -11,6 +14,10 @@ import seedu.address.logic.parser.exceptions.ParseException;
  */
 public class CommandParserTestUtil {
     public static final GuiState DEFAULT_STATE = GuiState.SUMMARY;
+    public static final String MESSAGE_WRONG_VIEW_SUMMARY =
+            String.format(GuiStateException.SWITCH_VIEW_MESSAGE, ListCommand.COMMAND_WORD + " mod");
+    public static final String MESSAGE_WRONG_VIEW_DETAILS =
+            String.format(GuiStateException.SWITCH_VIEW_MESSAGE, DetailCommand.COMMAND_WORD);
 
     /**
      * Asserts that the parsing of {@code userInput} by {@code parser} is successful and the command created
