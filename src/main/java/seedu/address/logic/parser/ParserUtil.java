b9881293bee99ff6295a91db4436d1ed97ebe1b4
Rohit <rajeshbhatrohit@gmail.com>
2021-10-27 13:04:47 +0800

    checkstyle fix

--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.module.Day;
 import seedu.address.model.module.Link;
