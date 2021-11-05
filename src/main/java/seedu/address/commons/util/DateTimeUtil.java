352791a0955392d02cde22e05a6a10a773c7c10b
luminousleek <68138671+luminousleek@users.noreply.github.com>
2021-11-05 21:49:33 +0800

    Make default resolver style strict

--- src/main/java/seedu/address/commons/util/DateTimeUtil.java
+++ src/main/java/seedu/address/commons/util/DateTimeUtil.java
@@ -2,16 +2,18 @@ package seedu.address.commons.util;
 
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
 
 public class DateTimeUtil {
 
     /**
-     * Returns a case-insensitive DateTimeFormatter according to the given pattern
+     * Returns a case-insensitive DateTimeFormatter according to the given pattern with a Strict resolver style
      */
     public static DateTimeFormatter buildFormatter(String pattern) {
         return new DateTimeFormatterBuilder()
                 .parseCaseInsensitive()
                 .appendPattern(pattern)
-                .toFormatter();
+                .toFormatter()
+                .withResolverStyle(ResolverStyle.STRICT);
     }
 }
